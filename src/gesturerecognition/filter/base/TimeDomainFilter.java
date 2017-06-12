b267c3ca172d5ea162226ccc53f6fdf4093c988e
daniamurad <daniamurad@gmail.com>
2017-06-12 15:52:56 +0800

    changed time domain filter

--- src/gesturerecognition/filter/base/TimeDomainFilter.java
+++ src/gesturerecognition/filter/base/TimeDomainFilter.java
@@ -4,9 +4,13 @@ public class TimeDomainFilter {
 	
 	public static float[] _buffer = new float [1];
 	
+	//This is a comments
 	public void calculateAverage(float[] buffer) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void Calculate(int something)
+	{}
 
 }
